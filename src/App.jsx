96557b9e547ce43f5287e3e545a3233259671cca
karlsome <ranilekarlkenneth@sasakicoating.com>
2026-03-31 16:30:10 +0900

    background changed to lavalamp

--- src/App.jsx
+++ src/App.jsx
@@ -54,6 +54,7 @@ function App() {
           <div className="aurora-blob aurora-blob-1" />
           <div className="aurora-blob aurora-blob-2" />
           <div className="aurora-blob aurora-blob-3" />
+          <div className="aurora-blob aurora-blob-4" />
         </div>
       )}
       <Sidebar activePage={activePage} onNavigate={(page) => navigate(`/${page}`)} />
